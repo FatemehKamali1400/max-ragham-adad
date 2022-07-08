d40ffa7e5f826d9c9eecd81a71c38ce9016cff17
Yamahdi@1984 <fatemeh.kamali198422@gmail.com>
2022-07-09 02:37:26 +0430

    tamrin9

---
+++ max ragham adad/Program.cs
@@ -0,0 +1,16 @@
+﻿int num, a, max=0;
+System.Console.WriteLine("Enter a number:");
+num=int.Parse(Console.ReadLine());
+while (num != 0)
+{
+    a = num % 10 ;
+   
+    if (max<=a)
+    {
+        max=a;
+    }
+    num=num / 10;
+}
+
+System.Console.WriteLine("max argham adad=" + max);
+ 
